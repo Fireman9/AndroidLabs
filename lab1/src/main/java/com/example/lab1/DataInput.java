f4b8b0aafef78a9cd544bd3f247e55c6b3050979
fireman9 <nazariystr@gmail.com>
2021-11-15 00:04:10 +0200

    added second lab

---
+++ lab1/src/main/java/com/example/lab1/DataInput.java
@@ -0,0 +1,75 @@
+package com.example.lab1;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class DataInput extends Fragment {
+
+    interface OnFragmentSendDataListener {
+        void onSendData(String mobileType, String company);
+    }
+
+    private OnFragmentSendDataListener fragmentSendDataListener;
+
+    String[] mobileTypes = {"Monoblock with numeric keypad", "Monoblock with flip", "Slider",
+            "Clamshells", "Monoblock with QWERTY-keyboard", "Touchscreen phone"};
+    String[] companies = {"Samsung", "Nokia", "Meizu", "Xiaomi", "Google", "Apple"};
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            fragmentSendDataListener = (OnFragmentSendDataListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString());
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_data_input, container, false);
+
+        Spinner spinner = view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_item, mobileTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        RadioGroup radioGroup = view.findViewById(R.id.radioGroup);
+        for (int i = 0; i < companies.length; i++) {
+            RadioButton radioButton = new RadioButton(getContext());
+            radioButton.setId(i);
+            radioButton.setText(companies[i]);
+            radioGroup.addView(radioButton);
+        }
+
+        Button submitButton = view.findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (spinner.getSelectedItem() != null && radioGroup.getCheckedRadioButtonId() != -1) {
+                    RadioButton radioButton = view.findViewById(radioGroup.getCheckedRadioButtonId());
+                    fragmentSendDataListener.onSendData(spinner.getSelectedItem().toString(), (String) radioButton.getText());
+                } else {
+                    Toast toast = Toast.makeText(getContext(),
+                            "Fill in all fields", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+        return view;
+    }
+}
--- lab1/src/main/java/com/example/lab1/MainActivity.java
+++ lab1/src/main/java/com/example/lab1/MainActivity.java
@@ -3,52 +3,20 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    String[] mobileTypes = {"Monoblock with numeric keypad", "Monoblock with flip", "Slider",
-            "Clamshells", "Monoblock with QWERTY-keyboard", "Touchscreen phone"};
-    String[] companies = {"Samsung", "Nokia", "Meizu", "Xiaomi", "Google", "Apple"};
+public class MainActivity extends AppCompatActivity implements DataInput.OnFragmentSendDataListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Spinner spinner = findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, mobileTypes);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        for (int i = 0; i < companies.length; i++) {
-            RadioButton radioButton = new RadioButton(this);
-            radioButton.setId(i);
-            radioButton.setText(companies[i]);
-            radioGroup.addView(radioButton);
-        }
     }
 
-    public void showResult(View view) {
-        Spinner spinner = findViewById(R.id.spinner);
-        RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        if (spinner.getSelectedItem() != null && radioGroup.getCheckedRadioButtonId() != -1) {
-            TextView textTypeResult = findViewById(R.id.TextTypeResult);
-            TextView textCompanyResult = findViewById(R.id.TextCompanyResult);
-            textTypeResult.setText(String.format("Type: %s", spinner.getSelectedItem().toString()));
-            RadioButton radioButton = findViewById(radioGroup.getCheckedRadioButtonId());
-            textCompanyResult.setText(String.format("Company: %s", radioButton.getText()));
-        } else {
-            Toast toast = Toast.makeText(this, "Fill in all fields", Toast.LENGTH_SHORT);
-            toast.show();
-        }
+    @Override
+    public void onSendData(String mobileType, String company) {
+        Result fragment = (Result) getSupportFragmentManager()
+                .findFragmentById(R.id.resultFragmentContainerView);
+        if (fragment != null)
+            fragment.setData(mobileType, company);
     }
 }
